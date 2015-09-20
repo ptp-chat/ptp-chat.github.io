64a9a82d992a4810da1788ac3c0c86d07641b455
Dustin Dowell <dustindowell22@gmail.com>
2015-09-20 05:28:39 -0500

    swapped the javascript ordered list for a pure css one

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,4 @@
 (function($) {
-  $(function() {
-    var rules = $('.rules li'),
-        rulesTotal = rules.length;
-
-    rules.each(function(index) {
-      index++;
-      var rule = $(this);
-      rule.attr('id', index);
-      rule.html('<span class="index">'+index+'</span><span class="content">'+rule.html()+'</span>');
-      if (rulesTotal - index) rule.after('<hr>');
-    });
-  });
-
   $(window).on('load hashchange', function(){
     if (window.location.hash){
       $('.selected').removeClass('selected');
