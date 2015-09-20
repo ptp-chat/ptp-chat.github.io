18e1fb53c376ad7e6ef55d7ca01940fefe751b20
JamenMarz <jamen.marz@gmail.com>
2015-09-20 00:52:16 -0600

    Add rule selection and more responsive.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,23 @@
 (function($) {
   $(function() {
-    var ruleList = function() {
-      $('.rules li').each(function(index) {
-        index++;
-        $(this).prepend('<span>'+index+'</span>');
-      });
-    };
-    ruleList();
+
+    var rules = $('.rules li'),
+        rulesTotal = rules.length;
+
+    rules.each(function(index) {
+      index++;
+      var rule = $(this);
+      rule.attr('id', index);
+      rule.html('<span class="index">'+index+'</span><span class="content">'+rule.html()+'</span>');
+      if (rulesTotal - index) rule.after('<hr>');
+    });
+  });
+
+  $(window).on('load hashchange', function(){
+    $('.selected').removeClass('.selected');
+    var selections = (window.location.hash).replace(/^[^0-9]/g, '').split(',');
+    selections.forEach(function(rule){
+      $('.rules li#' + rule).addClass('selected');
+    });
   });
 })(jQuery);
